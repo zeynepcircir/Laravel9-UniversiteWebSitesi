94c87c3d76b78ebc82398c1f6b3592ad7e0d583f
zeynepcircir <zynpcrcr@gmail.com>
2022-04-15 02:31:12 +0300

    Using Layout Master Page

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    //
+    public function index()
+    {
+        return view('home.index');
+    }
+
+    public function test()
+    {
+        return view('home.test');
+    }
+
+    public function test3()
+    {
+        return view('home.testlayout');
+    }
+
+    public function param($id,$number)
+    {
+        //echo "Parameter 1:", $id;
+        //echo "<br>Parameter 2:", $number;
+        //echo "<br>Sum Parameters :", $id+$number;
+        return view('home.test2',
+            [
+                'id' => $id,
+                'number' =>$number
+
+            ]);
+    
+    }
+            public function save(Request $request)
+        {
+            //echo "Save Function <br>";
+            //echo "First Name :",$_REQUEST["fname"]; 
+            //echo "Last Name :",$_REQUEST["lname"];
+            //return view('home.test2',
+            [
+                'fname' => $_REQUEST["fname"],
+                'lname' =>$_REQUEST["lname"]
+
+            ];
+        }                      
+
+
+    
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -10,12 +12,30 @@ use Illuminate\Support\Facades\Route;
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
-|
+| Test Comment
 */
+// 1- Do something in route
+Route::get('/hello', function () {
+    return 'Hello World';
+});
 
-Route::get('/', function () {
+// 2- Call view in route
+Route::get('/welcome', function () {
     return view('welcome');
 });
+
+// 3- Call Controller Function
+Route::get('/',[HomeController::class,'index'])->name('home');
+
+// 4- Route-> Controller->View
+Route::get('/test',[HomeController::class,'test'])->name('test');
+
+// 5- Route with parameters
+Route::get('/param/{id}/{number}',[HomeController::class,'param'])->name('param');
+// Route with save
+Route::post('/save',[HomeController::class,'save'])->name('save');
+
+
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
